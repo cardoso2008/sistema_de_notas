46ba9078cd5ffd82ce4adcbcaea3238c941dd877
GUILHERME SOARES CARDOSO <47400958828@info.local>
2026-03-16 21:25:37 -0300

    Inserção da função de gerar relatório

--- processamento.py
+++ processamento.py
@@ -1,1 +1,10 @@
+def gerar_relatorio(resultados, recuperacao, top_student, maior_media):
+    with open("resultado.txt", "w", encoding="utf-8") as f:
+        f.write("RELATÓRIO DE DESEMPENHO\n" + "="*25 + "\n")
+        for nome, media in resultados:
+            f.write(f"{nome}: {media:.2f}\n")
+        
+        f.write("\nRecuperação:\n" + ", ".join(recuperacao) if recuperacao else "Nenhum")
+        f.write(f"\n\nMelhor Aluno: {top_student} ({maior_media:.2f})")
 
+gerar_relatorio(resultados, recuperacao, top_student, maior_media)
